5f55e724cde0fefdc5030fafcdc35521ab03b0a1
packersblogger <packersblogger@gmail.com>
2019-10-28 13:53:58 -0600

    test

--- assets/python/games.py
+++ assets/python/games.py
@@ -1,44 +1,31 @@
 #!python
 import os
 import nflgame
+import re
+import fileinput
+import sys
 
 games = nflgame.games(2019, week=8)
+
 listofgames = []
 for g in games:
     listofgames.append(g)
     
+my_file = '_posts/2019-10-23-stevespicksweek8.md'
 
-my_file = '_posts/2019-10-23-steviespicksweek8.md'
+index = 0 
+def getgame(index):
+    return(listofgames[index])
 
-with open(my_file, 'r') as file:
-    lines = file.readlines()
+for line in fileinput.input(my_file, inplace=1): 
 
-# length = len(lines)
-# i =0
-def getgame():
-   
-    for index, item in enumerate(listofgames):
-        return item
-        item = index + 1
-
-
-starts = [lines.index(l) for l in lines if l.startswith('##')]
-# game = [listofgames for ga in listofgames]
-  
-for s in starts:
-    game = getgame()
-    lines[(s + 1)]= f'**Actual: {game}**\r\n\r\n'
-    #     break
-# while i < length:
-   
+    if re.search('^.*?\([^\d]*(\d+)[^\d]*\).*$', line):
         
-            
-#         
-            # i += 1
-            
-with open(my_file, 'w') as file:
-    file.writelines(lines)
-file.close()
+        updatedGame = getgame(index)
+        line = f"## **{updatedGame}** \r\n"
+        index += 1
+    sys.stdout.write(line)
+
         
 
 
