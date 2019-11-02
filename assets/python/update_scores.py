7b4486375fe027cb181d22bc61f8b33e627d5f79
packersblogger <packersblogger@gmail.com>
2019-11-02 13:53:38 -0600

    test update times schedule

--- assets/python/update_scores.py
+++ assets/python/update_scores.py
@@ -1,6 +1,7 @@
 #!python
 import os
 import nflgame
+
 import re
 import fileinput
 import sys
@@ -11,18 +12,31 @@ listofgames = []
 for g in games:
     listofgames.append(g)
     
+    
 my_file = '_posts/2019-10-30-steviespicksweek9.md'
 
 index = 0 
 def getgame(index):
     return(listofgames[index])
+def gettime(index):
+    gameTime = listofgames[index].time
+    if gameTime.is_pregame():  
+        return("{} ET".format(listofgames[index].schedule['time']))
+    else:
+        return(gameTime)
+    
+    
+
+    
 
 for line in fileinput.input(my_file, inplace=1): 
 
     if re.search('^.*?\([^\d]*(\d+)[^\d]*\).*$', line):
         
         updatedGame = getgame(index)
-        line = f"## **{updatedGame}** \r\n"
+        time = gettime(index)
+        
+        line = f"## **{updatedGame}-{time}** \r\n"
         index += 1
     sys.stdout.write(line)
 
