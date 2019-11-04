4fe112d36d17ab6307c7a352e3191c784eaf98e7
packersblogger <packersblogger@gmail.com>
2019-11-04 11:32:15 -0700

    test

--- assets/python/update_scores.py
+++ assets/python/update_scores.py
@@ -10,47 +10,49 @@ games = nflgame.games(2019, week=9)
 listofgames = []
 for g in games:
     listofgames.append(g)
-    if g.winner == g.away:
-        winningscore = g.score_away
-        losingscore = g.score_home
-        difference = winningscore - losingscore
+    
+    
+my_file = '_posts/2019-10-30-steviespicksweek9.md'
 
-        print (f'{g.away} by {difference}')
+index = 0 
+def getwinner(index):
+    if listofgames[index].game_over():
+
+        if listofgames[index].winner == listofgames[index].away:
+            winningscore = listofgames[index].score_away
+            losingscore = listofgames[index].score_home
+            difference = winningscore - losingscore
+
+            return (f'{listofgames[index].away} by {difference}')
+        else:
+            winningscore = listofgames[index].score_home
+            losingscore = listofgames[index].score_away
+            difference = winningscore - losingscore
+
+            return (f'{listofgames[index].home} by {difference}')
     else:
-        winningscore = g.score_home
-        losingscore = g.score_away
-        difference = winningscore - losingscore
+        return ('not final')
 
-        print (f'{g.home} by {difference}')  
-    # if g.winner:
-    #     print(f'{g.winner}')
-    #     continue
-    # else:
-    #     print('none')
-    
-    
-# my_file = '_posts/2019-10-30-steviespicksweek9.md'
 
-# index = 0 
-# def getgame(index):
-#     return(listofgames[index])
-# def gettime(index):
-#     gameTime = listofgames[index].time
-#     if gameTime.is_pregame():  
-#         return("{} ET".format(listofgames[index].schedule['time']))
-#     else:
-#         return(gameTime)
+def getgame(index):
+    return(listofgames[index])
+def gettime(index):
+    gameTime = listofgames[index].time
+    if gameTime.is_pregame():  
+        return("{} ET".format(listofgames[index].schedule['time']))
+    else:
+        return(gameTime)
     
-# for line in fileinput.input(my_file, inplace=1): 
-
-#     if re.search('^.*?\([^\d]*(\d+)[^\d]*\).*$', line):
+for line in fileinput.input(my_file, inplace=1): 
+    
+    if re.search('^.*?\([^\d]*(\d+)[^\d]*\).*$', line):
         
-#         updatedGame = getgame(index)
-#         time = gettime(index)
-        
-#         line = f"## **{updatedGame}-{time}** \r\n\r\n"
-#         index += 1
-#     sys.stdout.write(line)
+        updatedGame = getgame(index)
+        time = gettime(index)
+        winner = getwinner(index)
+        line = f"## **{updatedGame}-{time}**\r\n**{winner}** \r\n"
+        index += 1
+    sys.stdout.write(line)
 
         
 
